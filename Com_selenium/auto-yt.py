a225825a49d8f06d549da625d958031302a47a10
euapariciocoelho <faparicionc@ufpi.edu.br>
2023-02-27 11:33:47 -0300

    Update auto-yt.py

--- Com_selenium/auto-yt.py
+++ Com_selenium/auto-yt.py
@@ -1,9 +1,15 @@
+# preenchendo formulario
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
+'''
+    para pegar o elemento 'find_element' -> clica em inspecionar, clica na parte na tela que deseja usar, e copia o xpath
+    que vai aparecer após clicar no objeto selecionado
+    
+'''
 navegador.get('https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M') # link
 navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys('Aparicio')
 navegador.find_element('xpath', '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys('faparicionc@gmail.com')
